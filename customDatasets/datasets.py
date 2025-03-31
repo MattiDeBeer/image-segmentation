2a508af32498094dd547d1b901c7c79deaf7f8b9
matti de beer <mattidebeer@gmail.com>
2025-03-31 22:53:22 +0100

    Added dataset with label that corresponds to class number

--- customDatasets/datasets.py
+++ customDatasets/datasets.py
@@ -70,6 +70,47 @@ class ImageDataset(Dataset):
 
         return image, label
     
+class ImageDatasetClasses(Dataset):
+    # Initialize the dataset with specified parameters
+    def __init__(self, dataset="mattidebeer/Oxford-IIIT-Pet-Augmented", split='train'):
+        
+        if split not in ['train', 'validation', 'test']:
+            raise ValueError(f"split must be one of: 'train', 'validation', 'test'. You selected {split}")
+        
+        self.split = split
+
+        self.dataset = self.dataset = load_dataset(dataset,split=split)
+
+    # Return the total length of the dataset
+    def __len__(self):
+        return len(self.dataset)
+    
+    # Format a single datapoint by processing image and mask
+    def _format_datapoint(self,datapoint):
+        image = self._deserialize_numpy(datapoint['image'])
+        image = image.astype(np.float32) / 255.0
+        mask = self._deserialize_numpy(datapoint['mask'],shape=(256,256))
+        cat_mask = torch.from_numpy(np.where(mask == 38,1,0)).unsqueeze(0).long() #cat
+        dog_mask = torch.from_numpy(np.where(mask == 75,2,0)).unsqueeze(0).long() #dog
+
+        image = torch.from_numpy(image).permute(2,0,1)
+        label = cat_mask + dog_mask
+
+        return image, label
+    
+    # Helper function to deserialize numpy arrays from byte data
+    def _deserialize_numpy(self,byte_data, shape=(256,256,3), dtype=np.uint8):
+        return copy.deepcopy(np.frombuffer(byte_data, dtype=dtype).reshape(shape))
+
+    # Get a single item from the dataset
+    def __getitem__(self, idx):
+
+        # Get and format datapoint
+        datapoint = self.dataset[idx]
+        image, label = self._format_datapoint(datapoint)
+
+        return image, label
+    
 # Dataset class for handling image data with buffered loading
 class ImageDataset3Mask(Dataset):
     # Initialize the dataset with specified parameters
@@ -134,19 +175,20 @@ class ImageDataset3Mask(Dataset):
     
 class DummyDataset:
 
-    def __init__(self,image_channels=3,width=256,height=256,label_channels=2):
+    def __init__(self,image_channels=3,width=256,height=256,label_channels=2,length = 100):
         self.image_channels = image_channels
         self.width = width
         self.height = height
         self.label_channels = label_channels
         self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+        self.length = length
 
     def __len__(self):
-        return 100
+        return self.length
     
     def __getitem__(self,idx):
 
         image = torch.rand(self.image_channels,self.width,self.height).to(self.device)
-        label = torch.rand(self.label_channels,self.width,self.height).to(self.device)
+        label = torch.softmax(torch.rand(self.label_channels,self.width,self.height).to(self.device),dim=0)
 
         return image, label
