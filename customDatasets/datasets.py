90032f05e1730f5cefb7847785b7320ab1bfaa83
matti de beer <mattidebeer@gmail.com>
2025-03-31 13:42:39 +0100

    Added 3 class dataset, that holds background as it's own mask

--- customDatasets/datasets.py
+++ customDatasets/datasets.py
@@ -70,6 +70,68 @@ class ImageDataset(Dataset):
 
         return image, label
     
+# Dataset class for handling image data with buffered loading
+class ImageDataset3Mask(Dataset):
+    # Initialize the dataset with specified parameters
+    def __init__(self, dataset="mattidebeer/Oxford-IIIT-Pet-Augmented", split='train', transform=None, uncertianty_mask_coeff = 0):
+        
+        if split not in ['train', 'validation', 'test']:
+            raise ValueError(f"split must be one of: 'train', 'validation', 'test'. You selected {split}")
+        
+        self.split = split
+
+        assert uncertianty_mask_coeff >= 0 and uncertianty_mask_coeff <= 1, f"Uncertianty mask coefficient must be a value between 0 and 1, you inputted {uncertianty_mask_coeff}"
+        
+        self.uncertianty_mask_coeff = uncertianty_mask_coeff
+
+        self.dataset = self.dataset = load_dataset(dataset,split=split)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+
+    # Return the total length of the dataset
+    def __len__(self):
+        return len(self.dataset)
+    
+    # Format a single datapoint by processing image and mask
+    def _format_datapoint(self,datapoint):
+        image = self._deserialize_numpy(datapoint['image'])
+        image = image.astype(np.float32) / 255.0
+        mask = self._deserialize_numpy(datapoint['mask'],shape=(256,256))
+        cat_mask = torch.from_numpy(np.where(mask == 38,1,0)).unsqueeze(0) #cat
+        dog_mask = torch.from_numpy(np.where(mask == 75,1,0)).unsqueeze(0) #dog
+
+        cat_mask = cat_mask.to(self.device)
+        dog_mask = dog_mask.to(self.device)
+
+        if self.uncertianty_mask_coeff != 0:
+
+            uncertianty_mask = torch.from_numpy(np.where(mask == 255,1,0)).unsqueeze(0)
+            uncertianty_mask = uncertianty_mask.to(self.device)
+
+            if cat_mask.any():
+                cat_mask = cat_mask + self.uncertianty_mask_coeff * uncertianty_mask
+            else:
+                dog_mask = dog_mask + self.uncertianty_mask_coeff * uncertianty_mask
+
+        background_mask = torch.ones_like(cat_mask).to(self.device) - cat_mask - dog_mask
+        image = torch.from_numpy(image).to(self.device).permute(2,0,1)
+        label = torch.cat([cat_mask,dog_mask,background_mask],dim=0)
+
+        return image, label
+    
+    # Helper function to deserialize numpy arrays from byte data
+    def _deserialize_numpy(self,byte_data, shape=(256,256,3), dtype=np.uint8):
+        return copy.deepcopy(np.frombuffer(byte_data, dtype=dtype).reshape(shape))
+
+    # Get a single item from the dataset
+    def __getitem__(self, idx):
+
+        # Get and format datapoint
+        datapoint = self.dataset[idx]
+        image, label = self._format_datapoint(datapoint)
+
+        return image, label
+    
 class DummyDataset:
 
     def __init__(self,image_channels=3,width=256,height=256,label_channels=2):
