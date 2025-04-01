9e7f977e4f1dce99e1ea0b5c45dce7c111555597
matti de beer <mattidebeer@gmail.com>
2025-04-01 23:26:09 +0100

    updated depricated syntax in the mixed precision training code

--- scripts/train_distributed.py
+++ scripts/train_distributed.py
@@ -23,7 +23,7 @@ def loss_function(outputs,targets,criterion):
     return criterion(outputs,targets)
 
 def train(num_epochs, model, dataloader,rank, train_sampler,optimizer,criterion,save_location, validation_dataloader):
-    scaler = GradScaler()
+    scaler = GradScaler('cuda')
     for epoch in range(0,num_epochs):
 
         running_loss = 0
@@ -35,7 +35,7 @@ def train(num_epochs, model, dataloader,rank, train_sampler,optimizer,criterion,
 
             optimizer.zero_grad()
 
-            with autocast():
+            with autocast('cuda'):
                 outputs = model(images)
                 loss = loss_function(outputs,labels,criterion)
 
@@ -75,13 +75,14 @@ def validate(model, validation_dataloader, criterion, rank):
             inputs, targets = inputs.cuda(rank), targets.cuda(rank)  # Move data to device
             
             # Forward pass
-            outputs = model(inputs)
+            with autocast('cuda'):
+                outputs = model(inputs)
             
-            # Calculate different losses
-            hybrid_loss = criterion(outputs, targets)
-            iou_loss = IoULoss()(outputs, targets)
-            pixel_acc_loss = PixelAccuracyLoss()(outputs, targets)
-            dice_loss = DiceLoss()(outputs, targets)
+                # Calculate different losses
+                hybrid_loss = criterion(outputs, targets)
+                iou_loss = IoULoss()(outputs, targets)
+                pixel_acc_loss = PixelAccuracyLoss()(outputs, targets)
+                dice_loss = DiceLoss()(outputs, targets)
             
             # Track the losses
             running_val_loss += hybrid_loss.item()
