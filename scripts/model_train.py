7593fbea516fdf7ae237008093b88b7e3d0bb3a0
matti de beer <mattidebeer@gmail.com>
2025-04-01 15:21:24 +0100

    Updated model train to asses important validation metrics

--- scripts/model_train.py
+++ scripts/model_train.py
@@ -1,5 +1,5 @@
 
-from customDatasets.datasets import ImageDataset, DummyDataset, ImageDataset3Mask
+from customDatasets.datasets import ImageDataset, DummyDataset, ImageDataset3Mask, ImageDatasetClasses
 from models.UNet import UNet
 from torch.utils.data import DataLoader
 from models.helperFunctions import get_next_run_folder, save_training_info, write_csv_header,log_loss_to_csv
@@ -7,6 +7,7 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from tqdm import tqdm  # For progress bar
+from models.losses import HybridLoss, IoULoss, PixelAccuracyLoss, DiceLoss
 
 
 ###### Hyperperameters ###########
@@ -17,10 +18,10 @@ batch_size = 16
 
 uncertianty_mask_coefficient = 0.7
 
-model_save_file = "/tmp/im-seg-res/UNets3Mask"
-#dataset_loc = '../../Datasets/Oxford-IIIT-Pet-Augmented'
+model_save_file = "saved-models/Test"
+dataset_loc = '../../Datasets/Oxford-IIIT-Pet-Augmented'
 
-dataset_loc = "mattidebeer/Oxford-IIIT-Pet-Augmented" #uncomment to load remote dataset
+#dataset_loc = "mattidebeer/Oxford-IIIT-Pet-Augmented" #uncomment to load remote dataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 ##############################
@@ -30,8 +31,11 @@ torch.cuda.empty_cache()
 
 save_location = get_next_run_folder(model_save_file)
 
-train_dataset = ImageDataset3Mask(dataset=dataset_loc,split='train', uncertianty_mask_coeff=uncertianty_mask_coefficient)
-validation_dataset = ImageDataset3Mask(dataset=dataset_loc,split='validation', uncertianty_mask_coeff=0)
+train_dataset = DummyDataset(image_channels=3,label_channels=1, length = 32)
+validation_dataset = DummyDataset(image_channels=3,label_channels=1, length = 32)
+
+#train_dataset = ImageDatasetClasses(split='train')
+#validation_dataset = ImageDatasetClasses(split = 'validation')
 
 train_dataloader = DataLoader(train_dataset,batch_size = batch_size)
 validation_dataloader = DataLoader(validation_dataset,batch_size=batch_size)
@@ -40,11 +44,12 @@ model = torch.compile(model)
 model.to(device)  # Then move to GPU
 
 optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
-criterion = nn.functional.kl_div
+criterion = HybridLoss()
 
 save_training_info(model,
                    optimizer,
-                   criterion,train_dataloader,
+                   criterion,
+                   train_dataloader,
                    validation_dataloader,
                    save_location, 
                    extra_params = {'uncertianty_mask_coefficient' : uncertianty_mask_coefficient})
@@ -66,7 +71,7 @@ for epoch in tqdm( range(num_epochs), desc='Training', unit = 'Epoch', leave = F
         
         # Forward pass
         outputs = model(inputs)
-        loss = criterion(torch.nn.functional.log_softmax(outputs,dim=1), targets, reduction='batchmean')  # Compute the loss
+        loss = criterion(outputs, targets)  # Compute the loss
         
         # Backward pass and optimization
         loss.backward()
@@ -78,31 +83,47 @@ for epoch in tqdm( range(num_epochs), desc='Training', unit = 'Epoch', leave = F
     # Calculate average training loss and accuracy
     avg_train_loss = running_loss / len(train_dataloader)
     
-    print(f"Train Loss: {avg_train_loss:.4f}")
-    
     # Validation loop
     model.eval()  # Set the model to evaluation mode
     running_val_loss = 0.0
-    correct_val = 0
-    total_val = 0
+    running_iou_loss = 0.0
+    running_pixel_acc_loss = 0.0
+    running_dice_loss = 0.0
     
     with torch.no_grad():  # No gradients needed during validation
         for inputs, targets in tqdm(validation_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Validation",leave=False):
             inputs, targets = inputs.to(device), targets.to(device)  # Move data to device
             
-            # Forward pass (no need for backward or optimizer step)
+            # Forward pass
             outputs = model(inputs)
-            loss = criterion(torch.nn.functional.log_softmax(outputs,dim=1), targets , reduction='batchmean')  # Compute the loss
             
-            # Track the loss
-            running_val_loss += loss.item()
+            # Calculate different losses
+            hybrid_loss = criterion(outputs, targets)
+            iou_loss = IoULoss()(outputs, targets)
+            pixel_acc_loss = PixelAccuracyLoss()(outputs, targets)
+            dice_loss = DiceLoss()(outputs, targets)
+            
+            # Track the losses
+            running_val_loss += hybrid_loss.item()
+            running_iou_loss += iou_loss.item()
+            running_pixel_acc_loss += pixel_acc_loss.item()
+            running_dice_loss += dice_loss.item()
     
-    # Calculate average validation loss and accuracy
+    # Calculate average validation losses
     avg_val_loss = running_val_loss / len(validation_dataloader)
+    avg_iou_loss = running_iou_loss / len(validation_dataloader)
+    avg_pixel_acc_loss = running_pixel_acc_loss / len(validation_dataloader)
+    avg_dice_loss = running_dice_loss / len(validation_dataloader)
     
-    print(f"Validation Loss: {avg_val_loss:.4f}")
+    tqdm.write(f"Epoch: {epoch}")
+    tqdm.write(f"Train Loss: {avg_train_loss:.4f}")
+    tqdm.write(f"Validation Loss: {avg_val_loss:.4f}")
+    tqdm.write(f"Val IoU: {avg_iou_loss:.4f}")
+    tqdm.write(f"Val Pixel Accuracy: {avg_pixel_acc_loss:.4f}")
+    tqdm.write(f"Val Dice: {avg_dice_loss:.4f}")
+    tqdm.write('\n')
 
-    log_loss_to_csv(epoch,avg_train_loss,avg_val_loss,save_location)
+    log_loss_to_csv(epoch,avg_train_loss,avg_val_loss, avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss, save_location)
     
     torch.save(model.state_dict(), f'{save_location}model_{epoch+1}.pth')
 
