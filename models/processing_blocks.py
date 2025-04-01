956227f49d58f22a091a118a6f548c1a08186755
matti de beer <mattidebeer@gmail.com>
2025-04-01 20:43:04 +0100

    using slow preprocessor

--- models/processing_blocks.py
+++ models/processing_blocks.py
@@ -69,7 +69,7 @@ class ClipFeatureExtractor(nn.Module):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-        self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", use_fast = True)
+        self.clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", use_fast = False)
 
         self.train_clip = train
         self.train(train)
