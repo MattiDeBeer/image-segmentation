4ff47c1830fed228f202080c4e4886a81474d0ff
matti de beer <mattidebeer@gmail.com>
2025-04-03 22:37:49 +0100

    Added dataloader that does transforms on the GPU

--- customDatasets/datasets.py
+++ customDatasets/datasets.py
@@ -297,3 +297,117 @@ class ClassImageDataset(Dataset):
         return image, (mask, label)
         
 
+class ClassImageDatasetGPU(Dataset):
+
+    def __init__(self,dataset_loc = 'Data/Oxford-IIIT-Pet-Augmented', augmentations_per_datapoint = 2, split='validation'):
+        
+        if split not in ['train', 'validation', 'test']:
+            raise ValueError(f"split must be one of: 'train', 'validation', 'test'. You selected {split}")
+        
+        assert isinstance(augmentations_per_datapoint,int) and augmentations_per_datapoint >= 0, f"You must choose a positive integer for augmentations per datapoint, you choose: {augmentations_per_datapoint}"
+
+        try:
+            self.dataset = load_dataset(dataset_loc, split=split)
+        except Exception as e: 
+            if "doesn't exist on the Hub or cannot be accessed" in str(e):
+                print(f"Error: The dataset was not found locally")
+                download_huggingface_dataset("mattidebeer/Oxford-IIIT-Pet-Augmented",dataset_loc,split=split)
+                self.dataset = load_dataset(dataset_loc, split=split)
+            else:
+                print(f"An unexpected error occurred: {e}")
+
+        self.augmentations_per_datapoint = augmentations_per_datapoint + 1
+
+    def __len__(self):
+        return(len(self.dataset) * self.augmentations_per_datapoint)
+    
+    
+    def _deserialize_datapoint(self,datapoint):
+        image = self._deserialize_numpy(datapoint['image'])
+        mask = self._deserialize_numpy(datapoint['mask'],shape=(256,256))
+
+        image = torch.from_numpy(image).permute(2,0,1).float() / 255.0
+
+        background_mask = np.where(mask == 255, 1,0)
+        cat_mask = np.where(mask == 38, 1,0)
+        dog_mask = np.where(mask == 75,1,0)
+
+        if np.sum(cat_mask) > 0:
+            label = 0
+        else:
+            label = 1
+
+        segment_mask = cat_mask + dog_mask + background_mask
+
+        return image, torch.tensor(segment_mask).float(), torch.tensor([label]).float()
+    
+    def _deserialize_numpy(self,byte_data, shape=(256,256,3), dtype=np.uint8):
+        return copy.deepcopy(np.frombuffer(byte_data, dtype=dtype).reshape(shape))
+    
+    def __getitem__(self, idx):
+
+        image_index = idx // self.augmentations_per_datapoint
+
+        datapoint = self.dataset[image_index]
+        image, mask, label = self._deserialize_datapoint(datapoint)
+
+        return image, (mask, label)
+
+
+class CustomCollateFn:
+    def __init__(self, augmentations_per_datapoint):
+    
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.augmentations_per_datapoint = 2
+
+        self.image_transform  = torch.nn.Sequential(
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(90),
+
+            ### Image only transforms ###
+            transforms.ColorJitter(brightness=0.4, contrast=0.3, saturation=0.2, hue=0.2),
+            transforms.GaussianBlur(21)
+        ).to(self.device)
+        
+        self.mask_transform = torch.nn.Sequential(
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(90),
+        ).to(self.device)
+
+    def __call__(self, batch):
+
+        images, labels = zip(*batch)  # Unpack batch
+
+        images = torch.stack(images)
+        masks, class_labels = zip(*labels)
+
+        masks = torch.stack(masks)
+        class_labels = torch.stack(class_labels)
+
+        masks = masks.to(self.device)
+        images = images.to(self.device)
+        class_labels = class_labels.to(self.device)
+
+        saved_images = images[::self.augmentations_per_datapoint+1]
+        saved_masks = masks[::self.augmentations_per_datapoint+1]
+
+        #Generate a random seed
+        seed = torch.randint(0, 2**32, (1,)).item()
+
+        random.seed(seed)
+        torch.manual_seed(seed)
+        transformed_images = self.image_transform(images)
+
+        random.seed(seed)
+        torch.manual_seed(seed)
+        transformed_masks = self.mask_transform(masks.unsqueeze(1)).squeeze(1)
+
+        transformed_images[::self.augmentations_per_datapoint+1] = saved_images
+        transformed_masks[::self.augmentations_per_datapoint+1] = saved_masks
+
+        print(class_labels.size())
+
+        labels = (transformed_masks,class_labels)
+
+        return transformed_images, labels
