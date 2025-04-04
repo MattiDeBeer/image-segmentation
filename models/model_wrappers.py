829bed514c8f99361933b97241b6867d60d18761
matti de beer <mattidebeer@gmail.com>
2025-04-04 17:24:27 +0100

    added a distributed training model wrapper and training script

--- models/model_wrappers.py
+++ models/model_wrappers.py
@@ -135,48 +135,214 @@ class TrainingWrapper:
             running_pixel_acc_loss = 0.0
             running_dice_loss = 0.0
         
-        with torch.no_grad():  # No gradients needed during validation
-            for inputs, targets in tqdm(self.validation_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Validation",leave=False):
+            with torch.no_grad():  # No gradients needed during validation
+                for inputs, targets in tqdm(self.validation_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Validation",leave=False):
+                    inputs, targets = inputs.to(self.device, non_blocking=True), targets.to(self.device, non_blocking=True)
+
+                    with torch.autocast(self.device.type):
+                        # Forward pass
+                        outputs = self.model(inputs)
+                        
+                        # Calculate different losses
+                        hybrid_loss = self.criterion(outputs, targets)
+                        iou_loss = iou(outputs, targets)
+                        pixel_acc_loss = pixel_acc(outputs, targets)
+                        dice_loss = dice(outputs, targets)
+                    
+                    # Track the losses
+                    running_val_loss += hybrid_loss.item()
+                    running_iou_loss += iou_loss.item()
+                    running_pixel_acc_loss += pixel_acc_loss.item()
+                    running_dice_loss += dice_loss.item()
+            
+            # Calculate average validation losses
+            avg_val_loss = running_val_loss / len(self.validation_dataloader)
+            avg_iou_loss = running_iou_loss / len(self.validation_dataloader)
+            avg_pixel_acc_loss = running_pixel_acc_loss / len(self.validation_dataloader)
+            avg_dice_loss = running_dice_loss / len(self.validation_dataloader)
+            
+            tqdm.write(f"Epoch: {epoch}")
+            tqdm.write(f"Rate: {rate:.1f} datapoints/s")
+            tqdm.write(f"Train Loss: {avg_train_loss:.4f}")
+            tqdm.write(f"Validation Loss: {avg_val_loss:.4f}")
+            tqdm.write(f"Val IoU: {avg_iou_loss:.4f}")
+            tqdm.write(f"Val Pixel Accuracy: {avg_pixel_acc_loss:.4f}")
+            tqdm.write(f"Val Dice: {avg_dice_loss:.4f}")
+            tqdm.write('\n')
+
+            log_loss_to_csv(epoch,avg_train_loss,avg_val_loss, avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss, self.save_location)
+            
+            # Delete previous model save if it exists (keep only latest)
+            if epoch > 0:
+                prev_model_path = f'{self.save_location}model_{epoch}.pth'
+                if os.path.exists(prev_model_path):
+                    os.remove(prev_model_path)
+
+            # Save current model
+            torch.save(self.model.state_dict(), f'{self.save_location}model_{epoch+1}.pth')
+
+
+class DistributedTrainingWrapper:
+
+    def __init__(self,
+                 rank,
+                 model,
+                 train_dataloader,
+                 validation_dataloader,
+                 data_augmentor,
+                 num_workers = 0,
+                 batch_size = 100,
+                 save_location = None,   
+                 criterion_class = HybridLoss, 
+                 optimizer_class = optim.Adam,
+                 optimizer_args = {'lr': 0.001, 'weight_decay' : 1e-4},
+                 ):
+    
+
+        if num_workers > 0:
+            os.environ["OMP_NUM_THREADS"] = str(num_workers)
+            os.environ["MKL_NUM_THREADS"] = str(num_workers)
+
+        mp.set_start_method('spawn', force=True)
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+        torch.cuda.empty_cache()
+
+        if save_location is None:
+            save_location = "saved-models/" + model.__class__.__name__
+
+        self.save_location = get_next_run_folder(save_location)
+
+        self.batch_size = batch_size
+
+        self.model = model
+        self.train_dataloader = train_dataloader
+        self.validation_dataloader = validation_dataloader
+        self.data_augmentor = data_augmentor
+        self.rank = rank
+
+        self.optimizer = optimizer_class(model.parameters(), **optimizer_args)
+        self.criterion = criterion_class()
+
+        num_params = sum(p.numel() for p in self.model.parameters())
+
+        if self.rank == 0:
+            save_training_info(model,
+                            self.optimizer,
+                            self.criterion,
+                            self.train_dataloader,
+                            self.validation_dataloader,
+                            self.save_location, 
+                            extra_params = {'num_params' : num_params})
+
+
+
+    def train(self, num_epochs):
+
+        if self.rank == 0:
+            write_csv_header(self.save_location)
+
+        gradscaler = torch.GradScaler(self.device.type)
+
+        iou = IoU()
+        dice = Dice()
+        pixel_acc = PixelAccuracy()
+
+        for epoch in tqdm( range(num_epochs), desc='Training', unit = 'Epoch', leave = False):
+            
+            self.model.train()
+            running_loss = 0.0
+
+            start_time = time.time()
+            
+            # Training loop
+            for inputs, targets in tqdm(self.train_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Training", unit=' batch', leave=False):
                 inputs, targets = inputs.to(self.device, non_blocking=True), targets.to(self.device, non_blocking=True)
 
-                with torch.autocast(self.device.type):
-                    # Forward pass
-                    outputs = self.model(inputs)
-                    
-                    # Calculate different losses
-                    hybrid_loss = self.criterion(outputs, targets)
-                    iou_loss = iou(outputs, targets)
-                    pixel_acc_loss = pixel_acc(outputs, targets)
-                    dice_loss = dice(outputs, targets)
+                inputs, targets = self.data_augmentor(inputs,targets)
                 
-                # Track the losses
-                running_val_loss += hybrid_loss.item()
-                running_iou_loss += iou_loss.item()
-                running_pixel_acc_loss += pixel_acc_loss.item()
-                running_dice_loss += dice_loss.item()
-        
-        # Calculate average validation losses
-        avg_val_loss = running_val_loss / len(self.validation_dataloader)
-        avg_iou_loss = running_iou_loss / len(self.validation_dataloader)
-        avg_pixel_acc_loss = running_pixel_acc_loss / len(self.validation_dataloader)
-        avg_dice_loss = running_dice_loss / len(self.validation_dataloader)
-        
-        tqdm.write(f"Epoch: {epoch}")
-        tqdm.write(f"Rate: {rate:.1f} datapoints/s")
-        tqdm.write(f"Train Loss: {avg_train_loss:.4f}")
-        tqdm.write(f"Validation Loss: {avg_val_loss:.4f}")
-        tqdm.write(f"Val IoU: {avg_iou_loss:.4f}")
-        tqdm.write(f"Val Pixel Accuracy: {avg_pixel_acc_loss:.4f}")
-        tqdm.write(f"Val Dice: {avg_dice_loss:.4f}")
-        tqdm.write('\n')
+                self.optimizer.zero_grad()  # Zero gradients from the previous step
+                
+                # Forward pass
+                with torch.autocast(self.device.type):
+                    outputs = self.model(inputs)
+                    loss = self.criterion(outputs, targets)  # Compute the loss
+                
+                # Backward pass and optimization
+                gradscaler.scale(loss).backward()
+                gradscaler.step(self.optimizer)
+                gradscaler.update()
+                
+                # Track the loss
+                running_loss += loss.item()
 
-        log_loss_to_csv(epoch,avg_train_loss,avg_val_loss, avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss, self.save_location)
-        
-        # Delete previous model save if it exists (keep only latest)
-        if epoch > 0:
-            prev_model_path = f'{self.save_location}model_{epoch}.pth'
-            if os.path.exists(prev_model_path):
-                os.remove(prev_model_path)
+            end_time = time.time()
+            
+            # Calculate time per datapoint
+            time_per_epoch = end_time - start_time
+            total_datapoints = len(self.train_dataloader) * self.batch_size
+            rate = total_datapoints / time_per_epoch
 
-        # Save current model
-        torch.save(self.model.state_dict(), f'{self.save_location}model_{epoch+1}.pth')
+            # Calculate average training loss and accuracy
+            avg_train_loss = running_loss / len(self.train_dataloader)
+            
+            # Validation loop
+            self.model.eval()  # Set the model to evaluation mode
+            running_val_loss = 0.0
+            running_iou_loss = 0.0
+            running_pixel_acc_loss = 0.0
+            running_dice_loss = 0.0
+
+            torch.distributed.barrier()
+            
+            with torch.no_grad():  # No gradients needed during validation
+                for inputs, targets in tqdm(self.validation_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Validation",leave=False):
+                    inputs, targets = inputs.to(self.device, non_blocking=True), targets.to(self.device, non_blocking=True)
+
+                    with torch.autocast(self.device.type):
+                        # Forward pass
+                        outputs = self.model(inputs)
+                        
+                        # Calculate different losses
+                        hybrid_loss = self.criterion(outputs, targets)
+                        iou_loss = iou(outputs, targets)
+                        pixel_acc_loss = pixel_acc(outputs, targets)
+                        dice_loss = dice(outputs, targets)
+                    
+                    # Track the losses
+                    running_val_loss += hybrid_loss.item()
+                    running_iou_loss += iou_loss.item()
+                    running_pixel_acc_loss += pixel_acc_loss.item()
+                    running_dice_loss += dice_loss.item()
+            
+            # Calculate average validation losses
+            avg_val_loss = running_val_loss / len(self.validation_dataloader)
+            avg_iou_loss = running_iou_loss / len(self.validation_dataloader)
+            avg_pixel_acc_loss = running_pixel_acc_loss / len(self.validation_dataloader)
+            avg_dice_loss = running_dice_loss / len(self.validation_dataloader)
+            
+            tqdm.write(f"Epoch: {epoch}")
+            tqdm.write(f"Rate: {rate:.1f} datapoints/s")
+            tqdm.write(f"Train Loss: {avg_train_loss:.4f}")
+            tqdm.write(f"Validation Loss: {avg_val_loss:.4f}")
+            tqdm.write(f"Val IoU: {avg_iou_loss:.4f}")
+            tqdm.write(f"Val Pixel Accuracy: {avg_pixel_acc_loss:.4f}")
+            tqdm.write(f"Val Dice: {avg_dice_loss:.4f}")
+            tqdm.write('\n')
+
+            if self.rank == 0:
+
+                log_loss_to_csv(epoch,avg_train_loss,avg_val_loss, avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss, self.save_location)
+                
+                # Delete previous model save if it exists (keep only latest)
+                if epoch > 0:
+                    prev_model_path = f'{self.save_location}model_{epoch}.pth'
+                    if os.path.exists(prev_model_path):
+                        os.remove(prev_model_path)
+
+                # Save current model
+                torch.save(self.model.state_dict(), f'{self.save_location}model_{epoch+1}.pth')
+
+            torch.distributed.barrier()
+            
--- scripts/train_distributed.py
+++ scripts/train_distributed.py
@@ -1,166 +1,48 @@
 import torch
 import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
+from models.model_wrappers import DistributedTrainingWrapper
+from models.UNet import UNet
+from customDatasets.datasets import CustomImageDataset
+from models.processing_blocks import DataAugmentor
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
-from customDatasets.datasets import CustomImageDataset
-from models.UNet import UNet
-from torch.utils.data import DataLoader
-from models.helperFunctions import get_next_run_folder, save_training_info, write_csv_header,log_loss_to_csv
-from tqdm import tqdm 
-from models.losses import HybridLoss, IoU, PixelAccuracy, Dice
-from torch import autocast, GradScaler
-from models.processing_blocks import DataAugmentor
 
-
-#setup funciton
-def setup(rank, world_size):
-    """Initialize distributed process group"""
+def setup_ddp(rank, world_size):
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
-def loss_function(outputs,targets,criterion):
-    return criterion(outputs,targets)
+def cleanup_ddp():
+    dist.destroy_process_group()
 
-def train(num_epochs, model, dataloader,rank, train_sampler,optimizer,criterion,save_location, validation_dataloader, augmentor):
-    gradscaler = GradScaler('cuda')
-    for epoch in range(0,num_epochs):
+def main_ddp(rank, world_size):
+    setup_ddp(rank, world_size)
 
-        running_loss = 0
-        model.train()
-
-        train_sampler.set_epoch(epoch)
-        for images, labels in tqdm(dataloader, desc=f"Epoch {epoch+1}/{num_epochs} Training", unit=' batch', leave=False):
-            images, labels = images.cuda(rank), labels.cuda(rank)
-
-            images, labels = augmentor(images, labels)
-
-            optimizer.zero_grad()
-
-            with autocast(device_type='cuda'):
-                outputs = model(images)
-                loss = loss_function(outputs,labels,criterion)
-
-
-            gradscaler.scale(loss).backward()
-            gradscaler.step(optimizer)
-            gradscaler.update()
-            
-            running_loss += loss.item()
-        
-        val_loss, avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss  = validate(model,validation_dataloader,criterion,rank)
-        train_loss = running_loss / len(dataloader)
-
-        tqdm.write(f"Rank {rank}, Epoch {epoch}, Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}")
-
-        dist.barrier()
-
-        if rank == 0:
-            log_loss_to_csv(epoch,train_loss,val_loss,avg_pixel_acc_loss, avg_dice_loss, avg_iou_loss,save_location)
-            torch.save(model.state_dict(), f'{save_location}model_{epoch+1}.pth')
-
-            tqdm.write(f"Val IoU: {avg_iou_loss:.4f}")
-            tqdm.write(f"Val Pixel Accuracy: {avg_pixel_acc_loss:.4f}")
-            tqdm.write(f"Val Dice: {avg_dice_loss:.4f}")
-
-        
-
-def validate(model, validation_dataloader, criterion, rank):
-    model.eval()  # Set the model to evaluation mode
-    running_val_loss = 0.0
-    running_iou_loss = 0.0
-    running_pixel_acc_loss = 0.0
-    running_dice_loss = 0.0
-    
-    with torch.no_grad():  # No gradients needed during validation
-        for inputs, targets in tqdm(validation_dataloader, desc=f"Validation",leave=False):
-            inputs, targets = inputs.cuda(rank), targets.cuda(rank)  # Move data to device
-            
-            # Forward pass
-            with autocast(device_type = 'cuda'):
-                outputs = model(inputs)
-            
-                # Calculate different losses
-                hybrid_loss = criterion(outputs, targets)
-                iou_loss = IoU()(outputs, targets)
-                pixel_acc_loss = PixelAccuracy()(outputs, targets)
-                dice_loss = Dice()(outputs, targets)
-            
-            # Track the losses
-            running_val_loss += hybrid_loss.item()
-            running_iou_loss += iou_loss.item()
-            running_pixel_acc_loss += pixel_acc_loss.item()
-            running_dice_loss += dice_loss.item()
-    
-    # Calculate average validation losses
-    avg_val_loss = running_val_loss / len(validation_dataloader)
-    avg_iou_loss = running_iou_loss / len(validation_dataloader)
-    avg_pixel_acc_loss = running_pixel_acc_loss / len(validation_dataloader)
-    avg_dice_loss = running_dice_loss / len(validation_dataloader)
-
-    return avg_val_loss, avg_pixel_acc_loss,avg_dice_loss,avg_iou_loss
-
-#training loop
-def train_distributed(rank, world_size):
-
-    setup(rank, world_size)
-   
-    model = UNet(out_channels = 3)
-
-    num_epochs = 200
-    batch_size = 16
-
-    model_save_file = "saved-models/Test"
-
+    batch_size = 10
+    augmentations_per_datapoint = 4
     num_workers = 0
 
-    augmentations_per_datapoint = 4
+    model = UNet.to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
 
-    # Clear unused memory from the cache
-    torch.cuda.empty_cache()
+    DataAugmentor = DataAugmentor(augmentations_per_datapoint)
 
-    #load datasets
-    train_dataset = CustomImageDataset(split='train', cache=True, augmentations_per_datapoint=augmentations_per_datapoint)
-    val_dataset = CustomImageDataset(split='validation', cache=True)
+    train_dataset = CustomImageDataset(split="train",augmentations_per_datapoint=augmentations_per_datapoint)
+    validation_dataset = CustomImageDataset(split="validation",augmentations_per_datapoint=0)
 
     train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers, pin_memory=True)
 
-    val_sampler = DistributedSampler(val_dataset, num_replicas=world_size, rank=rank, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers, pin_memory=True)
+    val_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers, pin_memory=True)
 
-    #compile model and wrap with DDP
-    model = model.cuda(rank)  # Move to the correct device
-    model = DDP(model, device_ids=[rank])
+    trainer = DistributedTrainingWrapper(rank, ddp_model, train_dataloader, val_dataloader, num_workers=num_workers, batch_size=batch_size, augmentor=DataAugmentor)
+    trainer.train(10)
 
-    augmentor = DataAugmentor(augmentations_per_datapoint).cuda(rank)
-    augmentor = DDP(augmentor, device_ids=[rank])
+    cleanup_ddp()
 
-    #Optimizer and loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    criterion = HybridLoss()
-
-    save_location = get_next_run_folder(model_save_file)
-
-    if rank == 0:
-        save_training_info(model,
-                   optimizer,
-                   criterion,
-                   train_dataloader,
-                   val_dataloader,
-                   save_location, 
-                   extra_params = {})
-
-        write_csv_header(save_location)
-
-
-    train(num_epochs,model,train_dataloader,rank, train_sampler,optimizer,criterion,save_location,val_dataloader, augmentor)
-
-    #cleanup
-    dist.destroy_process_group()
-
-#multi gpu entry point
 if __name__ == "__main__":
     world_size = torch.cuda.device_count()
-    mp.spawn(train_distributed, args=(world_size,), nprocs=world_size)
+    mp.spawn(main_ddp, args=(world_size,), nprocs=world_size)
 
