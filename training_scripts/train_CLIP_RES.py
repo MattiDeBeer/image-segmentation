d8bea97b3b207888d400f45454db4000373d3c37
matti de beer <mattidebeer@gmail.com>
2025-04-04 18:23:41 +0100

    Added model specific training scripts

---
+++ training_scripts/train_CLIP_RES.py
@@ -0,0 +1,20 @@
+from models.model_wrappers import TrainingWrapper
+from models.CLIP_models import ClipResSegmentationModel
+from customDatasets.datasets import DummyDataset, CustomImageDataset
+
+if __name__ == '__main__':
+    
+    Trainer = TrainingWrapper(
+        model_class= ClipResSegmentationModel,
+        model_arguments={'out_channels' : 3},
+        model_compilation_args = {'max_autotune' : True},
+        num_workers = 4,
+        batch_size = 80,
+        train_dataset_class = CustomImageDataset,
+        train_dataset_args = {'split' : 'test', 'cache' : True},
+        validation_dataset_class= CustomImageDataset,
+        validation_dataset_args = {'split' : 'validation', 'cache' : True},
+        augmentations_per_datapoint = 4,
+    )
+
+    Trainer.train(200)
