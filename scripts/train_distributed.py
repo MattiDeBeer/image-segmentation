502cc4bc5fba92075832ff909618f7e736941589
matti de beer <mattidebeer@gmail.com>
2025-04-04 18:12:51 +0100

    updated trainer

--- scripts/train_distributed.py
+++ scripts/train_distributed.py
@@ -2,67 +2,48 @@ import torch
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
+import os
 from models.model_wrappers import DistributedTrainingWrapper
 from models.UNet import UNet
 from customDatasets.datasets import CustomImageDataset
 from models.processing_blocks import DataAugmentor
 from torch.utils.data import DataLoader, DistributedSampler
-import os
+import torch.nn as nn
 
-def setup_ddp():
-    # Get rank and world_size from environment variables set by torchrun
-    rank = dist.get_rank()
-    world_size = dist.get_world_size()
+def setup_ddp(rank, world_size):
     local_rank = int(os.environ["LOCAL_RANK"])
-
-    # Set up CUDA device for this rank
+    os.environ["MASTER_ADDR"] = os.environ["SLURM_NODELIST"] if "SLURM_NODELIST" in os.environ else "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(local_rank)
-
-    # Initialize the process group for DDP (NCCL backend for GPUs)
-    dist.init_process_group(backend="nccl", rank=rank, world_size=world_size)
-
-    return rank, world_size, local_rank
+    return local_rank
 
 def cleanup_ddp():
     dist.destroy_process_group()
 
 def main_ddp():
-    # Setup DDP and get rank, world_size, local_rank
-    rank, world_size, local_rank = setup_ddp()
-
-    batch_size = 16
-    augmentations_per_datapoint = 4
-    num_workers = 0
-
-    # Initialize the model and move it to the correct GPU
-    model = UNet().to(local_rank)
+    rank = int(os.environ['RANK'])
+    world_size = int(os.environ['WORLD_SIZE'])
+    local_rank = setup_ddp(rank, world_size)
+    model = UNet()
+    model = model.to(local_rank)
     ddp_model = DDP(model, device_ids=[local_rank])
-
-    # Set up the data augmentor
+    augmentations_per_datapoint = 4
     data_augmentor = DataAugmentor(augmentations_per_datapoint)
-
-    # Set up datasets and dataloaders
+    batch_size = 10
+    num_workers = 4
     train_dataset = CustomImageDataset(split="train", augmentations_per_datapoint=augmentations_per_datapoint)
     validation_dataset = CustomImageDataset(split="validation", augmentations_per_datapoint=0)
-
-    # DistributedSampler ensures each process gets a different subset of the data
     train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers, pin_memory=True)
-
-    val_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+    val_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank, shuffle=False)
     val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers, pin_memory=True)
-
-    # Initialize the training wrapper
     trainer = DistributedTrainingWrapper(rank, ddp_model, train_dataloader, val_dataloader, data_augmentor, num_workers=num_workers, batch_size=batch_size)
     trainer.train(2)
-
-    # Cleanup DDP
     cleanup_ddp()
 
 if __name__ == "__main__":
-    # Use torchrun to spawn multiple processes
-    world_size = int(os.environ["WORLD_SIZE"])  # This will be set by torchrun automatically
-    mp.spawn(main_ddp, nprocs=world_size, args=())
+    main_ddp()
 
 # Note: This script should be run using torchrun or similar command to handle distributed training.
 # For example:
