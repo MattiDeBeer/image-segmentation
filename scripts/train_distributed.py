fa2a8a068d22fa2ed3283f05c84186c53dd2cb4a
matti de beer <mattidebeer@gmail.com>
2025-04-04 17:34:08 +0100

    updated trainer for slurm

--- scripts/train_distributed.py
+++ scripts/train_distributed.py
@@ -8,17 +8,27 @@ from customDatasets.datasets import CustomImageDataset
 from models.processing_blocks import DataAugmentor
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader, DistributedSampler
+import os
 
 def setup_ddp(rank, world_size):
+    rank = int(os.environ["SLURM_PROCID"])
+    world_size = int(os.environ["SLURM_NTASKS"])
+    local_rank = int(os.environ["SLURM_LOCALID"])
+
+    os.environ["MASTER_ADDR"] = os.environ["SLURM_NODELIST"]
+    os.environ["MASTER_PORT"] = "12355"  # Any open port works
+
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
-    torch.cuda.set_device(rank)
+    torch.cuda.set_device(local_rank)
+    
+    return rank, world_size, local_rank
 
 def cleanup_ddp():
     dist.destroy_process_group()
 
-def main_ddp(rank, world_size):
+def main_ddp():
 
-    setup_ddp(rank, world_size)
+    rank, world_size, local_rank = setup_ddp()
 
     batch_size = 10
     augmentations_per_datapoint = 4
@@ -27,7 +37,7 @@ def main_ddp(rank, world_size):
     model = UNet.to(rank)
     ddp_model = DDP(model, device_ids=[rank])
 
-    DataAugmentor = DataAugmentor(augmentations_per_datapoint)
+    data_augmentor = DataAugmentor(augmentations_per_datapoint)
 
     train_dataset = CustomImageDataset(split="train",augmentations_per_datapoint=augmentations_per_datapoint)
     validation_dataset = CustomImageDataset(split="validation",augmentations_per_datapoint=0)
@@ -38,12 +48,11 @@ def main_ddp(rank, world_size):
     val_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank, shuffle=True)
     val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers, pin_memory=True)
 
-    trainer = DistributedTrainingWrapper(rank, ddp_model, train_dataloader, val_dataloader, num_workers=num_workers, batch_size=batch_size, augmentor=DataAugmentor)
+    trainer = DistributedTrainingWrapper(rank, ddp_model, train_dataloader, val_dataloader, num_workers=num_workers, batch_size=batch_size, augmentor=data_augmentor)
     trainer.train(2)
 
     cleanup_ddp()
 
 if __name__ == "__main__":
-    world_size = torch.cuda.device_count()
-    mp.spawn(main_ddp, args=(world_size,), nprocs=world_size)
+    main_ddp()
 
