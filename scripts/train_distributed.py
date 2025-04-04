15666702b55c4aac3e7b27494b76a431e502c2f3
matti de beer <mattidebeer@gmail.com>
2025-04-04 22:20:34 +0100

    updated ddp code

--- scripts/train_distributed.py
+++ scripts/train_distributed.py
@@ -10,48 +10,52 @@ from models.processing_blocks import DataAugmentor
 from torch.utils.data import DataLoader, DistributedSampler
 import torch.nn as nn
 
-def setup_ddp(rank, world_size):
-    if "SLURM_NODELIST" in os.environ:
-        os.environ["MASTER_ADDR"] = os.environ["SLURM_NODELIST"]
-    else:
-        os.environ["MASTER_ADDR"] = "localhost"  # Ensure this is localhost for single-node
+def setup(rank, world_size):
+    dist.init_process_group(
+        backend="nccl",
+        init_method="env://",
+        rank=rank,
+        world_size=world_size
+    )
+    torch.cuda.set_device(rank)
 
-    os.environ["MASTER_PORT"] = "12355"  # You can pick any open port
-
-    # Initialize DDP
-    dist.init_process_group("nccl", rank=rank, world_size=world_size)
-    local_rank = int(os.environ["LOCAL_RANK"])  # The device to use for each process
-    torch.cuda.set_device(local_rank)
-    torch.backends.cudnn.benchmark = True  # Enable benchmark mode for faster training
-    torch.backends.cudnn.deterministic = False  # Disable deterministic mode for faster training
-    torch.backends.cudnn.enabled = True  # Enable cuDNN
-    torch.cuda.empty_cache()  # Clear cache to avoid memory issues
-    print(f"Rank {rank} initialized DDP on device {local_rank}")
-    return local_rank
-
-def cleanup_ddp():
+def cleanup():
     dist.destroy_process_group()
 
 def main_ddp():
-    rank = int(os.environ['RANK'])
-    world_size = int(os.environ['WORLD_SIZE'])
-    local_rank = setup_ddp(rank, world_size)
+    rank = int(os.environ["RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
+
+    setup(rank, world_size)
+
+
+
     model = UNet()
-    model = model.to(local_rank)
-    ddp_model = DDP(model, device_ids=[local_rank])
+    model = model.to(rank)
+    ddp_model = DDP(model, device_ids=[rank])
+
     augmentations_per_datapoint = 4
     data_augmentor = DataAugmentor(augmentations_per_datapoint)
+
     batch_size = 10
     num_workers = 4
+    
     train_dataset = CustomImageDataset(split="train", augmentations_per_datapoint=augmentations_per_datapoint)
+
     validation_dataset = CustomImageDataset(split="validation", augmentations_per_datapoint=0)
+
     train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
+
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler, num_workers=num_workers, pin_memory=True)
+
     val_sampler = DistributedSampler(validation_dataset, num_replicas=world_size, rank=rank, shuffle=False)
+
     val_dataloader = DataLoader(validation_dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers, pin_memory=True)
+
     trainer = DistributedTrainingWrapper(rank, ddp_model, train_dataloader, val_dataloader, data_augmentor, num_workers=num_workers, batch_size=batch_size)
+
     trainer.train(2)
-    cleanup_ddp()
+    cleanup()
 
 if __name__ == "__main__":
     main_ddp()
