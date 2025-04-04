d1f99df2563ae971dd10a05f33d00c3128dfebf6
matti de beer <mattidebeer@gmail.com>
2025-04-04 18:38:05 +0100

    fixed input issue

--- training_scripts/train_CLIP_Unet.py
+++ training_scripts/train_CLIP_Unet.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     Trainer = TrainingWrapper(
         model_class= ClipUnet,
         model_arguments={'out_channels' : 3},
-        model_compilation_args = {'max-autotune' : True},
+        model_compilation_args = {'mode' : 'max-autotune'},
         num_workers = 4,
         batch_size = 80,
         train_dataset_class = CustomImageDataset,
--- training_scripts/train_Unet.py
+++ training_scripts/train_Unet.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     Trainer = TrainingWrapper(
         model_class= UNet,
         model_arguments={'out_channels' : 3},
-        model_compilation_args = {'max-autotune' : True},
+        model_compilation_args = {'mode' : 'max-autotune'},
         num_workers = 4,
         batch_size = 100,
         train_dataset_class = CustomImageDataset,
