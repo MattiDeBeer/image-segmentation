8b6c615e8da7051fed7ca61957804ac527a765e7
matti de beer <mattidebeer@gmail.com>
2025-04-05 14:41:36 +0100

    Added a larger UNet

--- models/UNet.py
+++ models/UNet.py
@@ -47,6 +47,51 @@ class UNet(nn.Module):
 
         return self.activation(out)
 
+class LargeUNet(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, activation = nn.Identity()):
+        super().__init__()
+        
+        self.input = nn.Conv2d(in_channels, 32, kernel_size=1, padding=0)
+       
+        # Encoder
+        self.enc1 = ConvBlockDownsample(32, 64)   # /2
+        self.enc2 = ConvBlockDownsample(64, 128)           # /4
+        self.enc3 = ConvBlockDownsample(128, 256)         # /8
+        self.enc4 = ConvBlockDownsample(256, 512)         # /16
+        
+        # Bottleneck
+        self.bottleneck = ConvBlock(512, 1024)             # /16
+        
+        self.dec1 = ConvBlockUpsampleSkip(1024, 512)           # /8
+        self.dec2 = ConvBlockUpsampleSkip(512, 256)            # /4
+        self.dec3 = ConvBlockUpsampleSkip(256, 128)            # /2
+        self.dec4 = ConvBlockUpsampleSkip(128, 64)             # /1
+        self.dec5 = ConvBlockUpsampleSkip(64, 32) 
+
+        self.out = nn.Conv2d(32, out_channels, kernel_size=1, padding=0)
+
+        self.activation = activation
+
+
+    def forward(self, x):
+        # Encoder
+        input = self.input(x)
+        enc1 = self.enc1(input)
+        enc2 = self.enc2(enc1)
+        enc3 = self.enc3(enc2)
+        enc4 = self.enc4(enc3)
+     
+        bottleneck = self.bottleneck(enc4)
+
+        dec1 = self.dec1(bottleneck, enc4)
+        dec2 = self.dec2(dec1, enc3)
+        dec3 = self.dec3(dec2, enc2)
+        dec4 = self.dec4(dec3, enc1)
+        dec5 = self.dec5(dec4, input)
+        out = self.out(dec5)
+
+        return self.activation(out)
+    
 class SegmentationClassificationUNet(nn.Module):
     def __init__(self, in_channels=3, activation = nn.Identity()):
         super().__init__()
@@ -103,9 +148,10 @@ class SegmentationClassificationUNet(nn.Module):
         return self.activation(out), classifier_out
 
 if __name__ == "__main__":
-    model = SegmentationClassificationUNet(in_channels=3)
-    mask, pred_class = model(torch.randn(1,3,256,256))
+    model = LargeUNet()
+    out = model(torch.randn(1,3,256,256))
 
-    print(f"mask size:{mask.size()}")
-    print(f"class size: {pred_class.size()}")
+    print(out.size())
+
+
     
