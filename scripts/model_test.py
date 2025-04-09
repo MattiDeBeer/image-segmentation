b6498acb1c2012a2984d31b8719d72410887e8dd
[Jamie Borland] <[s2059547]@sms.ed.ac.uk>
2025-04-09 17:46:42 +0100

    tesitng model

--- scripts/model_test.py
+++ scripts/model_test.py
@@ -1,14 +1,21 @@
-
+import torch
+import torch.nn as nn
 from models.model_wrappers import TestWrapper
-from models.UNet import UNet, LargeUNet
-from models.CLIP_models import ClipUnet, ClipResSegmentationModel, ClipAutoencoder
+from models.pre_trained import SegmentationModel, SegmentationDecoderSkip
 from customDatasets.datasets import CustomImageDataset
+from models.autoencoder import Autoencoder 
+
+autoencoder = Autoencoder()
+autoencoder.load_state_dict(torch.load("saved-models/Autoencoders/model_179/model_179.pth"), map_location="cpu")
+# Freeze encoder weights
+for param in autoencoder.encoder.parameters():
+    param.requires_grad = False
 
 tester = TestWrapper(
-    model=LargeUNet(),
+    model=SegmentationModel(autoencoder.encoder, SegmentationDecoderSkip(out_channels=2)),
     test_dataset_class=CustomImageDataset,
     batch_size=16,
-    model_load_location="results/models/LargeUNet/best_model.pth",
+    model_load_location="saved-models/pre_trained_autoencoder/pre_trained_autoencoder_best_model.pth",
     test_dataset_args = {'split':'test','augmentations_per_datapoint' : 0, 'cache' : False},
 )
 
